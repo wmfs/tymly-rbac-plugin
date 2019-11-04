d0f9d65a39f14bd4c9b08c19b37f042d3d96e20b
Meena.Hoda <meena.hoda@wmfs.net>
2019-11-04 08:45:21 +0000

    test: update for new state machine

--- test/show-permissions-state-machine-test.js
+++ test/show-permissions-state-machine-test.js
@@ -34,6 +34,9 @@ describe('RBAC Permissions State Machine', function () {
       },
       tymly_rbacGrantRoleMembershipForm_1_0: {
         '*': [ 'tymly_rbacAdmin' ]
+      },
+      tymly_viewRoleMemberships_1_0: {
+        '*': [ 'tymly_rbacAdmin' ]
       }
     }
   }
